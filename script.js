fd606d71f762e324428152b8af25f30dbf63ed4f
Андрей <forandrew@ya.ru>
2025-11-03 02:24:00 +0300

    notify2

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // script.js
 
 /* begin notify *******************/
-/* функция - замыкание для создания уведомлений */
-function makeNotify() {
+// функция - замыкание для создания уведомлений 
+function closureMakeNotify() {
 
     let notifyDiv;
     let notifyTimeout;
 
+    // сбрасывает таймер и удаляет уведомление из DOM 
     function closeNotify() {
         if(notifyTimeout) {
             clearTimeout(notifyTimeout);
@@ -18,10 +19,7 @@ function makeNotify() {
         }
     };
 
-    return function(
-            message /* текст уведомления */ 
-            , status /* стиль уведомления: 'info','positive','negative' */
-            , timeout = 3500 /* таймаут для скрытия ms, 0 = не скрывать */ ) {
+    return function(message, status, timeout = 3500) {
 
         if(['info','positive','negative'].indexOf(status) === -1) status = 'info';
         if(isNaN(timeout) || timeout < 0 ) timeout = 3500;
@@ -36,10 +34,9 @@ function makeNotify() {
         if(!notifyDiv) {
             // создаем блок уведомлений
             notifyDiv = document.createElement('div');
-            notifyDiv.classList.add('notify-div');
             // вставляем кнопку
             notifyDiv.append(document.createElement('i'));
-            notifyDiv.firstChild.innerText = 'x';
+            notifyDiv.firstChild.innerText = '✕';
             notifyDiv.firstChild.addEventListener('click', closeNotify);
             // вставляем блок для текста
             notifyDiv.append(document.createElement('div'));
@@ -47,6 +44,8 @@ function makeNotify() {
             document.body.append(notifyDiv);
         }
 
+        // обновляем классы
+        notifyDiv.className = 'notify-div '+status;
         // обновляем текст
         notifyDiv.lastChild.innerText = message;
         // устанавливаем таймер закрытия
@@ -54,6 +53,10 @@ function makeNotify() {
     }
  
 }
-/* функция для вызова уведомления */
-const notify = makeNotify();
+// функция для вызова уведомления 
+// параметры:
+// message - текст уведомления 
+// status - стиль уведомления: 'info','positive','negative' 
+// timeout = 3500 - таймаут для скрытия ms, 0 = не скрывать  
+const notify = closureMakeNotify();
 /* end notify *********************/
